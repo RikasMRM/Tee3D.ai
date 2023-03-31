03b4393f61e7fdd8f67b0950f18bc9ba83320779
RikasMRM <rikasrkf@gmail.com>
2023-04-01 01:20:52 +0530

    backend chaneg

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
           <motion.div className="home-content" {...headContainerAnimation}>
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+              Tee3D.ai <br className="xl:block hidden" />
               </h1>
             </motion.div>
             <motion.div
@@ -37,7 +37,7 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-                Create your unique and exclusive shirt with our brand-new 3D
+                Create your unique and exclusive T-shirt with our brand-new 3D
                 customization tool. <strong>Unleash your imagination</strong>{" "}
                 and define your own style.
               </p>
